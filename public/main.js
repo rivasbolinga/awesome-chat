9c13279ecf0a3af96c819d6132a156b968778a76
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-09-05 14:47:59 +0200

    Declare global variables from the DOM

--- public/main.js
+++ public/main.js
@@ -2,6 +2,11 @@ const socket = io();
 
 
 const clientsTotal = document.getElementById('clients-total');
+const messageContainer= document.getElementById('message-container');
+const nameInput = document.getElementById('name-input');
+const messageForm = document.getElementById('message-form');
+const messageInput = document.getElementById('message-input');
+
 socket.on('clients-total', (data)=> {
  clientsTotal.innerText = `Total clients: ${data}`
 })
